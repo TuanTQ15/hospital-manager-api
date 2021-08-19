63e4668816856491b5c545e7a891f828898fd4f9
TuanTQ15 <truongquoctuan61@gmail.com>
2021-08-19 11:43:40 +0700

    updated oauthen2

--- core/utility/oauth2.py
+++ core/utility/oauth2.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException,status,Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from . import token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login")
 
 async def get_current_user(data: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
