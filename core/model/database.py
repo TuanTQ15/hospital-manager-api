3f84971ba701abcccbcbb2b7788bbd5899b9e0eb
TuanTQ15 <83053023+TuanTQ15@users.noreply.github.com>
2022-09-15 01:33:00 +0700

    Update database.py

--- core/model/database.py
+++ core/model/database.py
@@ -2,8 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import os
-from dotenv import load_dotenv
-load_dotenv(".env")
+
 
 DATABASE_URL=os.environ["URI"]
 
